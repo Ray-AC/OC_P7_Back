35901b85cd4e3368d701ff96ab3f0d24bab44c32
Ray AC <ac.ray2000@gmail.com>
2024-02-26 00:29:02 +0100

    import model de prédiction, ajout liste client, ajout prédiction live

--- main.py
+++ main.py
@@ -8,20 +8,31 @@ import pickle
 import lightgbm as lgb
 from lightgbm import LGBMClassifier
 import joblib
+import json
+
+import gc
+from contextlib import contextmanager
+import time
+from lightgbm import LGBMClassifier
+import warnings
+from sklearn.model_selection import KFold, StratifiedKFold, train_test_split
+from sklearn.metrics import roc_auc_score
+
 
 app = FastAPI() #check query parameters
 
 final_dataframe = pd.read_csv("D:/Downloads/final_dataframe.csv")
 predict_df = pd.read_csv("D:/Downloads/predict.csv")
-'''best_lgb = joblib.load('D:/Downloads/best_lightgbm_model.pkl')'''
+best_lgb = joblib.load('D:/Downloads/best_lightgbm_model.pkl')
 
 @app.get("/")
 async def root():
     return "Vérification d'enregistrement"
 
-'''@app.get("/liste_client")
-async def liste_client():
-    return predict_df['sk-id-curr'].values'''
+@app.get("/get_unique_client_ids")
+async def get_unique_client_ids():
+    unique_client_ids = predict_df['sk-id-curr'].unique().tolist()
+    return unique_client_ids
 
 @app.get("/prediction_client")
 async def prediction_client(client_id: int):
@@ -36,3 +47,19 @@ async def prediction_client(client_id: int):
     client_data = client_row.to_dict(orient='records')[0]
     
     return client_data
+
+@app.get("/prediction_client_live")
+async def prediction_client_live(client_id: int):
+    # Vérifier si le client_id est présent dans predict_df['sk-id-curr']
+    if client_id not in predict_df['sk-id-curr'].values:
+        raise HTTPException(status_code=404, detail="Client ID not found")
+
+    # Obtenir les données du client correspondant du DataFrame predict_df
+    client_data = final_dataframe[final_dataframe['sk-id-curr'] == client_id].drop(columns=['target', 'sk-id-curr', 'index'])
+
+    # Effectuer la prédiction en direct
+    prediction = best_lgb.predict(client_data).tolist()
+    prediction_dict = {'prediction': prediction}
+
+    # Vous pouvez retourner le résultat de la prédiction comme vous le souhaitez, par exemple :
+    return prediction_dict
