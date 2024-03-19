7210d73c25a3480bfa4e31f953db745eb4565220
Ray AC <ac.ray2000@gmail.com>
2024-03-19 06:32:30 +0100

    deploiement cloud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import os
 import lime
 import pickle #
 import base64
@@ -189,3 +190,12 @@ async def interpratibilite(sk_id_curr_value: int):
     buffer.seek(0)
     explanation_base64 = base64.b64encode(buffer.getvalue()).decode()
     return explanation_base64
+
+# Récupérer le port fourni par Heroku
+port = int(os.environ.get('PORT', 8000))
+
+# Modifier l'appel à FastAPI pour utiliser le port récupéré
+if __name__ == "__main__":
+    # Utiliser uvicorn pour démarrer le serveur FastAPI sur le port spécifié
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=port)
